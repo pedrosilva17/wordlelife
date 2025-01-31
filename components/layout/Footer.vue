3fd34715411c8bcfac25a9b14bdee3f9d54b485c
Pedro Silva <pedroafsuper@gmail.com>
2025-01-31 20:33:33 +0100

    update author website domain name

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -5,7 +5,7 @@
 		Made with <UIcon name="i-mdi-heart" dynamic class="text-primary-500" /> by
 		<ULink
 			target="_blank"
-			to="https://pedrosilvadev.pt"
+			to="https://frompedrosilva.com"
 			class="font-bold text-primary-600 dark:text-primary-400"
 			>Pedro Silva</ULink
 		>
